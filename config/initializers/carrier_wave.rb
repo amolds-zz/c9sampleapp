8761fd516d18bbd2583fd7c10c637b839f719d5a
amolds <amolds@hotmail.com>
2015-12-04 05:54:50 +0000

    Updated CarrierWave config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,5 +7,6 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    config.s3_region = 'us-west-2'
   end
 end
